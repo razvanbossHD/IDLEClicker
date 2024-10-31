916c106102016e2e10d170b4c35f08eb3a67a8a4
RID-developer <radutuionutdorin@gmail.com>
2024-10-31 23:57:58 +0200

    Chestii

--- src/main/java/battle/BMenu.java
+++ src/main/java/battle/BMenu.java
@@ -1,1 +1,14 @@
 package battle;
+import graphics.*;
+public class BMenu{
+    public static void UPG()
+    {
+    Buttons.add(Battle.up::VAL,"Value",200,100,50,50);
+    Buttons.add(Battle.up::HP,"Difficulty",200,100,50,200);
+    Buttons.add(Battle.up::DAM,"Damage",200,100,50,350);
+    Buttons.add(Battle.up::SPD,"AtkSpd",200,100,50,500);
+    Buttons.add(Battle.up::CHN,"CritChan",200,100,50,650);
+    Buttons.add(Battle.up::CHN,"CritDam",200,100,50,800);
+    }
+
+}
--- src/main/java/battle/Battle.java
+++ src/main/java/battle/Battle.java
@@ -4,15 +4,17 @@ import time.*;
 import javafx.scene.layout.Pane;
 public class Battle{
     public static double money = 0;
+    public static Upgrades up = new Upgrades();
     static Pane pane = Ui.pane;
     public static void start()
     {
     pane.getChildren().clear();
+    BMenu.UPG();
     Select.selecter();
     Loop.m = 1;
     }
     public static void loop()
     {
-
+    System.out.println(up.val);
     }
 }
